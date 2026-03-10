a85f96ac4a5462daabaae47c13800ac8921ed685
Ankush Saggu <ankush22004@gmail.com>
2026-03-10 15:45:52 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+for(let i=1;i<=20;i++){
+
+let level=document.createElement("div")
+
+level.className="levelBtn"
+level.innerText=i
+
+level.onclick=()=>{
+
+startGame(i)
+
+}
+
+levelContainer.appendChild(level)
+
+}
