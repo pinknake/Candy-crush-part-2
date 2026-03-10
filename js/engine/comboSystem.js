3cf9e229d1c918d2e59b53aa323cf07ea3f7ed2b
Ankush Saggu <ankush22004@gmail.com>
2026-03-10 16:15:22 +0530

    Create comboSystem.js

---
+++ js/engine/comboSystem.js
@@ -0,0 +1,17 @@
+let combo=0
+
+function increaseCombo(){
+
+combo++
+
+document.getElementById("comboValue").innerText=combo
+
+}
+
+function resetCombo(){
+
+combo=0
+
+document.getElementById("comboValue").innerText=combo
+
+}
