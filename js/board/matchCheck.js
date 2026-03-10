bb44e370792654852888e13ecb3596dea9802e39
Ankush Saggu <ankush22004@gmail.com>
2026-03-10 15:55:27 +0530

    Create matchCheck.js

---
+++ js/board/matchCheck.js
@@ -0,0 +1,45 @@
+function checkMatches(){
+
+const candies=document.querySelectorAll(".candy")
+
+// horizontal
+
+for(let i=0;i<64;i++){
+
+if(i%8>5) continue
+
+let c1=candies[i]
+let c2=candies[i+1]
+let c3=candies[i+2]
+
+if(
+c1.style.background==c2.style.background &&
+c2.style.background==c3.style.background
+){
+
+removeCandy(c1,c2,c3)
+
+}
+
+}
+
+// vertical
+
+for(let i=0;i<48;i++){
+
+let c1=candies[i]
+let c2=candies[i+8]
+let c3=candies[i+16]
+
+if(
+c1.style.background==c2.style.background &&
+c2.style.background==c3.style.background
+){
+
+removeCandy(c1,c2,c3)
+
+}
+
+}
+
+}
