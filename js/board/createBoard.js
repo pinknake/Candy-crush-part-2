7ee0500e2fc08abc9a2261f1f969e072ba34b0aa
Ankush Saggu <ankush22004@gmail.com>
2026-03-10 15:48:53 +0530

    Create createBoard.js

---
+++ js/board/createBoard.js
@@ -0,0 +1,31 @@
+const boardSize=8
+
+const colors=[
+"red",
+"yellow",
+"blue",
+"green",
+"purple"
+]
+
+function createBoard(){
+
+const board=document.getElementById("gameBoard")
+
+board.innerHTML=""
+
+for(let i=0;i<boardSize*boardSize;i++){
+
+let candy=document.createElement("div")
+
+candy.className="candy"
+
+let randomColor=colors[Math.floor(Math.random()*colors.length)]
+
+candy.style.background=randomColor
+
+board.appendChild(candy)
+
+}
+
+}
